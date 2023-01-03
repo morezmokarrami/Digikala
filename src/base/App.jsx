0243bc7002fd220209dfb53ba6a57cf24a5046d7
mohammadrezamok <mokarrami.jsg@gmail.com>
2023-01-03 22:31:28 +0330

    first commit

---
+++ src/base/App.jsx
@@ -0,0 +1,10 @@
+import {Route, Routes} from "react-router-dom";
+
+export const App = () => {
+
+    return (
+        <Routes>
+            <Route path={'/namava'}></Route>
+        </Routes>
+    )
+}
